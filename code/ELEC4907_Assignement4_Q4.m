0842d972b0aa80afb5db020e697d24620118705b
Matthew Lazarus <matthewlazarus@cmail.carleton.ca>
2019-04-07 15:08:26 -0400

    Updated Q4

--- code/ELEC4907_Assignement4_Q4.m
+++ code/ELEC4907_Assignement4_Q4.m
@@ -4,8 +4,13 @@
 
 %% Part 4
 % In order to deal with the non-linear element in the circuit, a new column
-% matrix is introduced into the equation. Then, in order to solve the
-% system, the jacobian of the matrix would need to be calculated. Then, the 
+% matrix ('f(x)') is introduced into the equation (Gx +f(x) = b -- DC
+% Case). Then, in order to solve the
+% system, the jacobian of the matrix would need to be calculated.
+% The jacobian is added to the derivative of the new column matrix (for the
+% imaginary values). The sum is then divided by the sum of G*x+f(x)-b,
+% giving the change in the result (delta X)
+% Then, the 
 % Newton Raphson method is applied until the change in resulting values is 
-% sufficiently small. Then, the time can be advanced and the
+% sufficiently small. The time can be advanced and the
 % jacobian+newton raphson method can be applied again.
